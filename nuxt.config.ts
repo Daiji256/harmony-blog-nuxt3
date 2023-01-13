d010e7e92e8afb294c5cbd1cf1c824aad9497500
Daiji <butty.main@gmail.com>
2023-01-13 21:43:10 +0900

    アップデート

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	target: 'static',
 	css: ['modern-css-reset'],
 	modules: ['@nuxt/content'],
 	runtimeConfig: {
