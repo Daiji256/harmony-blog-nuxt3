065026ae8fb4ae4c9930ddf00f43733d47b4e38a
Daiji <butty.main@gmail.com>
2023-01-18 08:42:04 +0900

    rehype にして，span を入れれるようにした

---
+++ modules/rehype-kumi/index.js
@@ -0,0 +1,34 @@
+import flatMap from 'unist-util-flatmap';
+
+export default function plugin() {
+    function transformer(tree) {
+        flatMap(tree, function (node) {
+            if (node.type !== 'text') return [node];
+            if (node.value !== 'hoge') return [node];
+            return [
+                {
+                    type: 'text',
+                    value: 'text '
+                },
+                {
+                    type: 'element',
+                    tagName: 'span',
+                    properties: {
+                        className: ['kumi-aki-4']
+                    },
+                    children: [
+                        {
+                            type: 'text',
+                            value: 'span'
+                        }
+                    ]
+                },
+                {
+                    type: 'text',
+                    value: ' text'
+                }
+            ];
+        });
+    }
+    return transformer;
+}
