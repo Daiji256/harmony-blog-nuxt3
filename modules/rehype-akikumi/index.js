78313519224818ec23685815d8636b953a13658f
Daiji <butty.main@gmail.com>
2023-01-21 22:34:41 +0900

    和欧間のアキを追加

--- modules/rehype-akikumi/index.js
+++ modules/rehype-akikumi/index.js
@@ -1,34 +1,53 @@
-import flatMap from 'unist-util-flatmap';
+import flatMap from "unist-util-flatmap";
 
 export default function rehypeAkikumi() {
-    function transformer(tree) {
-        flatMap(tree, function (node) {
-            if (node.type !== 'text') return [node];
-            if (node.value !== 'hoge') return [node];
-            return [
-                {
-                    type: 'text',
-                    value: 'text '
-                },
-                {
-                    type: 'element',
-                    tagName: 'span',
-                    properties: {
-                        className: ['kumi-aki-4']
-                    },
-                    children: [
-                        {
-                            type: 'text',
-                            value: 'span'
-                        }
-                    ]
-                },
-                {
-                    type: 'text',
-                    value: ' text'
-                }
-            ];
-        });
+  const JAPANESE_WESTERN_REGEXP =
+    /([ぁ-んァ-ヶ一-龠ー])([0-9A-Za-zÀ-žÀ-žͰ-ϿЀ-ӿ])/g;
+  const WESTERN_JAPANESE_REGEXP =
+    /([0-9A-Za-zÀ-žÀ-žͰ-ϿЀ-ӿ])([ぁ-んァ-ヶ一-龠ー])/g;
+
+  const adjustAki = (tree) => {
+    flatMap(tree, (node) => {
+      return adjustMixedAki(node, JAPANESE_WESTERN_REGEXP);
+    });
+    flatMap(tree, (node) => {
+      return adjustMixedAki(node, WESTERN_JAPANESE_REGEXP);
+    });
+  };
+
+  const adjustMixedAki = (node, regexp) => {
+    if (node.type !== "text") return [node];
+    let ret = [];
+    let text = node.value;
+    while (true) {
+      const idx = text.search(regexp);
+      if (idx < 0) {
+        ret.push(makeText(text));
+        break;
+      }
+      ret.push(makeText(text.slice(0, idx + 1)));
+      ret.push(makeSpan("akikumi-aki-4"));
+      text = text.slice(idx + 1);
     }
-    return transformer;
+    return ret;
+  };
+
+  const makeText = (value) => {
+    return { type: "text", value: value };
+  };
+
+  const makeSpan = (className, value = null) => {
+    let children = [];
+    if (value !== null) {
+      children = [makeText(value)];
+    }
+    return {
+      type: "element",
+      tagName: "span",
+      properties: { className: [className] },
+      children: children,
+    };
+  };
+
+  return adjustAki;
 }
