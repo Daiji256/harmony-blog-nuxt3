eeea79b3b882870146dfadf03fce40bdad952ca2
Daiji <butty.main@gmail.com>
2023-02-01 21:04:44 +0900

    不要なスクリプトを削除

--- convert_webp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# `imagemagick` is required
-
-images=`find public/images -type f -name \*.jpg -or -name \*.png`
-for image in $images
-do
-  convert $image -resize 640x396^ -quality 75 -define webp:method=6 "${image%.*}.webp"
-  convert $image -resize 1200x630^ -gravity center -crop 1200x630+0+0 -quality 75 "${image%.*}-ogp.jpg"
-done
