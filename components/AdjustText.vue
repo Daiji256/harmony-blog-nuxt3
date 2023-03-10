7ddde5363e254bddca5fe2e7876a5bc99fd73273
Daiji <butty.main@gmail.com>
2023-03-10 22:30:50 +0900

    AdjustText を追加

---
+++ components/AdjustText.vue
@@ -0,0 +1,21 @@
+<template>
+  <span v-html="adjustTextHtml"></span>
+</template>
+
+<script setup lang="ts">
+import { unified } from 'unified';
+import rehypeParse from 'rehype-parse';
+import rehypeQuotes from 'rehype-quotes';
+import rehypeAdjustAki from 'rehype-adjust-aki';
+import rehypeStringify from 'rehype-stringify';
+
+type Props = { text: string | undefined };
+const { text } = defineProps<Props>();
+const adjustTextHtml = unified()
+  .use(rehypeParse)
+  .use(rehypeQuotes)
+  .use(rehypeAdjustAki)
+  .use(rehypeStringify)
+  .processSync(text)
+  .toString();
+</script>
