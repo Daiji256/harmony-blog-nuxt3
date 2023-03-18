646c3006e622d3fff4d8ada616c6ab0f3888a6ab
Daiji <butty.main@gmail.com>
2023-03-18 17:18:31 +0900

    タグ一覧ページを作成

---
+++ pages/tags.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="tags">
+    <NuxtLink class="tag" v-for="tag in tags" to="/"><!-- TODO: リンク先を設定-->
+      <div class="tag-content">
+        <AdjustText class="tag-name" v-bind:text="tag.name" />
+        <AdjustText class="tag-count" v-bind:text="tag.count" />
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variable";
+
+.tags {
+  margin: 16px 16px 8px 16px;
+
+  .tag {
+    display: inline-block;
+    margin: 0 8px 16px 0;
+    text-decoration: none;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+    @media (hover: hover) {
+      &:hover {
+        .tag-content {
+          background-color: rgba($color-on-surface-variant, $state-hover);
+          box-shadow: $my-box-shadow-level-2;
+        }
+      }
+    }
+
+    &:active {
+      .tag-content {
+        background-color: rgba($color-on-surface-variant, $state-pressed);
+      }
+    }
+
+    .tag-content {
+      @extend .font-body-large;
+      display: flex;
+      align-items: center;
+      border-radius: 8px;
+      min-height: 32px;
+      padding: 0 16px;
+      color: $color-on-surface;
+      background-color: $color-surface;
+      box-shadow: $my-box-shadow-level-1;
+
+      .tag-name {
+        margin-right: 4px;
+      }
+
+      .tag-count {
+        &::before {
+          content: "(";
+        }
+
+        &::after {
+          content: ")";
+        }
+      }
+    }
+  }
+}
+</style>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+const siteName = runtimeConfig.siteName;
+const title = `タグ一覧 - ${siteName}`;
+useHead({
+  title: title,
+});
+
+const posts = await queryContent('posts')
+  .where({ '_draft': false })
+  .only(['tags'])
+  .find();
+const tagsCount: number[] = [posts].flat().map(post => post.tags).flat().reduce(
+  (prev, current) => {
+    prev[current] = (prev[current] || 0) + 1;
+    return prev;
+  }, {},
+);
+const tags = Object.entries(tagsCount)
+  .map(([name, count]) => ({ name, count }))
+  .sort((a, b) => { return b.count - a.count })
+  .map(({ name, count }) => ({ name, count: `${count}` }));
+</script>
