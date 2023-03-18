5b88801078dbc12392e9b94a26e05839d2eebce5
Daiji <butty.main@gmail.com>
2023-03-18 20:47:40 +0900

    順番を変更

--- pages/post-list/[id].vue
+++ pages/post-list/[id].vue
@@ -5,11 +5,10 @@
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
 const siteName = runtimeConfig.siteName;
+const route = useRoute();
 const title = `すべての投稿 - ${siteName}`;
+const id = Number(route.params.id) || 1;
 useHead({
   title: title,
 });
-
-const route = useRoute();
-const id = Number(route.params.id) || 1;
 </script>
