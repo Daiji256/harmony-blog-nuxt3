042ad0373bf42cf5a0a15cf881369be1f19d89cf
Daiji256 <daiji.szk@gmail.com>
2023-08-05 16:10:52 +0900

    色を修正

--- components/LoadingIndicator.vue
+++ components/LoadingIndicator.vue
@@ -12,7 +12,7 @@
   width: 100%;
   height: calc(4px * v-bind(isShow));
   color: $color-primary;
-  background-color: elevation($color-surface, $elevation-level-5);
+  background-color: $color-surface-container-highest;
   background-image: linear-gradient(to right, transparent 50%, currentColor 50%, currentColor 60%, transparent 60%, transparent 71.5%, currentColor 71.5%, currentColor 84%, transparent 84%);
   animation: linear infinite 2s indeterminate;
   transition: height 0.25s;
