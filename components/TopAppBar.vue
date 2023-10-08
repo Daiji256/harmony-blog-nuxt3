8b6332cb6ad54f1f711d362ac02bc3da7d1fe386
Daiji256 <daiji.szk@gmail.com>
2023-10-08 11:33:05 +0900

    Revert "TopAppBar で i18n の文言を参照"
    This reverts commit 246c34bc637ca188fea2a39a2299b30c94865e08.

--- components/TopAppBar.vue
+++ components/TopAppBar.vue
@@ -3,7 +3,7 @@
     <div class="header-wrapper">
       <NuxtLink class="title" to="/">
         <div class="title-content">
-          <AdjustText v-bind:text="$t('siteName')" />
+          <AdjustText v-bind:text="siteName" />
         </div>
       </NuxtLink>
       <div class="trailing-menu" v-on:click="onMenuClick">
@@ -193,6 +193,8 @@ header {
 </style>
 
 <script setup lang="ts">
+const _runtimeConfig = useRuntimeConfig();
+const siteName = _runtimeConfig.public['siteName'];
 const isShowMenu = ref(false);
 const isAtTop = ref(true);
 
