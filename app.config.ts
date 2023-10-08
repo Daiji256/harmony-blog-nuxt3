dff039b21c48ca48047d0cd090dd1d672711dd73
Daiji256 <daiji.szk@gmail.com>
2023-10-08 11:47:18 +0900

    appConfig を追加

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+    strings: {
+        siteName: "Harmony Blog",
+        postsTitle: "すべての投稿",
+        postsDescription: "投稿一覧です。",
+        tagsTitle: "すべてのタグ",
+        tagsDescription: "全ての投稿のタグ一覧です。",
+        tagPageDescription: "の投稿の投稿一覧です。",
+    },
+    limitPerPage: 10,
+});
