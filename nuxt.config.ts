732de2b67dfc67cf376853b34188e68b73633ad9
Daiji256 <daiji.szk@gmail.com>
2023-10-08 11:54:32 +0900

    runtimeConfig を削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,15 +39,4 @@ export default defineNuxtConfig({
       preload: ["c", "js"],
     },
   },
-  runtimeConfig: {
-    public: {
-      siteName: "Harmony Blog",
-      postsTitle: "すべての投稿",
-      postsDescription: "投稿一覧です。",
-      tagsTitle: "すべてのタグ",
-      tagsDescription: "全ての投稿のタグ一覧です。",
-      tagPageDescription: "の投稿の投稿一覧です。",
-      limitPerPage: 10,
-    },
-  },
 })
