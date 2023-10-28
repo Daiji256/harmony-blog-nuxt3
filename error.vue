f49954bb2e0fbc25f830f342e8a1505d1650a326
Daiji256 <daiji.szk@gmail.com>
2023-10-28 17:04:01 +0900

    エラー表示の仮実装

---
+++ error.vue
@@ -0,0 +1,13 @@
+<template>
+  <TopAppBar />
+  {{ statusCode }}
+  {{ statusMessage }}
+  <FooterMenu />
+</template>
+
+<script setup lang="ts">
+type Props = { error: Object };
+const { error } = defineProps<Props>();
+const statusCode = 'statusCode' in error ? `${error.statusCode}` : 'unknown';
+const statusMessage = 'statusMessage' in error ? `${error.statusMessage}` : '';
+</script>
